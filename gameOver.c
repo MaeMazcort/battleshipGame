e090ca12481ab5a85c5a59f569b1a8d49880a6c0
MaeMazcort <mazcortmae@gmail.com>
2023-05-02 02:47:40 -0600

    finished program

--- gameOver.c
+++ gameOver.c
@@ -7,10 +7,10 @@
 int main(int argc, char const *argv[]) {
     int number = atoi(argv[1]);
 
-    printf(ANSI_COLOR_CYAN "\n\n\nCONGRATULATIONS! The player %d won\n", number);
+    printf(ANSI_COLOR_CYAN "\n\n\nCONGRATULATIONS! The player %d won\n\n\n", number);
     printf(ANSI_COLOR_RESET "\n");
     sleep(5);
 
-    
+
     return 0;
 }
